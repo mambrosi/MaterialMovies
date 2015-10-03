5cb4be1c426c23afe883add47d37ab203c7ecd92
Marcos Ambrosi <marcosambrosi@gmail.com>
2015-10-03 12:09:22 -0300

    Created Application class

--- app/src/main/java/marcosambrosi/mmovies/MoviesApplication.java
+++ app/src/main/java/marcosambrosi/mmovies/MoviesApplication.java
@@ -1,7 +1,15 @@
 package marcosambrosi.mmovies;
 
+import android.app.Application;
+
 /**
  * Created by marcosambrosi on 10/3/15.
  */
-public class MoviesApplication {
+public class MoviesApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
 }
