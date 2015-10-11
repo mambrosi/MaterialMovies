9a4f510f6cc49b1828d7905ce5707f258134ba71
Marcos Ambrosi <marcosambrosi@gmail.com>
2015-10-11 20:30:25 -0300

    Added current config to preferences

--- app/src/main/java/marcosambrosi/mmovies/MoviesApplication.java
+++ app/src/main/java/marcosambrosi/mmovies/MoviesApplication.java
@@ -1,11 +1,13 @@
 package marcosambrosi.mmovies;
 
 import android.app.Application;
-import android.app.Service;
-import android.content.pm.ApplicationInfo;
+import android.content.SharedPreferences;
+
+import com.google.gson.Gson;
 
 import marcosambrosi.mmovies.model.Configuration;
 import marcosambrosi.mmovies.network.ServiceController;
+import marcosambrosi.mmovies.util.Constants;
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -16,9 +18,14 @@ import retrofit.client.Response;
 public class MoviesApplication extends Application {
 
 
+    private SharedPreferences mSharedPreferences;
+
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mSharedPreferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
 
         checkIfCallConfigurationServiceNeeded();
     }
@@ -28,16 +35,33 @@ public class MoviesApplication extends Application {
             @Override
             public void success(Configuration configuration, Response response) {
                 if (configuration != null) {
-
+                    addPreference(Constants.PREFERENCES.CONFIG,
+                            new Gson().toJson(configuration,
+                                    Configuration.class));
                 }
             }
 
             @Override
             public void failure(RetrofitError error) {
                 if (error != null) {
-                    
+
                 }
             }
         });
+    }
+
+
+    public void addPreference(String key, String value) {
+        if (mSharedPreferences != null) {
+            mSharedPreferences.edit().putString(key, value).apply();
+        }
+    }
+
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        mSharedPreferences = null;
     }
 }
--- app/src/main/java/marcosambrosi/mmovies/util/Constants.java
+++ app/src/main/java/marcosambrosi/mmovies/util/Constants.java
@@ -29,4 +29,7 @@ public class Constants {
     public static final String API_KEY = "2ff37dad067cc303b43f42c955f94193";
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
 
+    public static class PREFERENCES {
+        public static final String CONFIG = "marcosambrosi.mmovies.preferences.config";
+    }
 }
